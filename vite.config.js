e0dd5ce5df67e515e38a464592537f7a02991e39
Osoapy <oosoapy@gmail.com>
2025-06-20 16:08:23 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/rpg-chainsaw-man-react/', // substitua pelo nome exato do seu repositório
-});
+  plugins: [react()],
+  base: '/rpg-chainsaw-man-react/', // muito importante isso aqui!
+})
